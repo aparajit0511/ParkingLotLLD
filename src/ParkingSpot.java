d8d4b440164a4f3a87ff7dd794acc005752dcfbc
aparajit0511 <aparajit35@gmail.com>
2026-09-01 18:57:57 +0530

    parking spot and parking ticket

--- src/ParkingSpot.java
+++ src/ParkingSpot.java
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime;
+
 public class ParkingSpot {
     private String spotId;
     private int floorNumber;
@@ -33,6 +35,28 @@ public class ParkingSpot {
 
     public void setParkingSpotType(ParkingSpotType parkingSpotType){
         this.parkingSpotType = parkingSpotType;
+    }
+
+    public boolean isOccupied(Vehicle vehicle){
+        if(vehicle == null){
+            return false;
+        }
+        return true;
+    }
+
+    public String parkAVehicle(Vehicle vehicle){
+        if(!isOccupied(vehicle)){
+            ParkingTicket parkingTicket = new ParkingTicket("TCK123",vehicle,getParkingSpotType(), LocalDateTime.now());
+            return parkingTicket.getTicketId();
+        }
+        return "Occupied";
+    }
+
+    public boolean unparkAVehicle(Vehicle vehicle){
+        if(isOccupied(vehicle)){
+            return this.vehicle == null;
+        }
+        return false;
     }
 
 }
--- src/ParkingTicket.java
+++ src/ParkingTicket.java
@@ -3,10 +3,10 @@ import java.time.LocalDateTime;
 public class ParkingTicket {
     private String ticketId;
     public Vehicle vehicle;
-    public ParkingSpot parkingSpot;
+    public ParkingSpotType parkingSpot;
     private LocalDateTime entryTime;
 
-    public ParkingTicket(String ticketId,Vehicle vehicle,ParkingSpot parkingSpot,LocalDateTime entryTime){
+    public ParkingTicket(String ticketId, Vehicle vehicle, ParkingSpotType parkingSpot, LocalDateTime entryTime){
         this.ticketId = ticketId;
         this.vehicle = vehicle;
         this.parkingSpot = parkingSpot;
